89d75539c57f9ec95ae169e75bc4ecef8afcb6e4
mkpi-pm <mkpi@pm.me>
2022-08-19 16:36:33 +0100

    add HTML structure for sidebar with active menu

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 import "./App.css";
 
 const App = () => {
+  const activeMenu = true;
+
   return (
     <div>
       <BrowserRouter>
@@ -21,6 +23,18 @@ const App = () => {
               </button>
             </TooltipComponent>
           </div>
+          {activeMenu ? (
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
+              Sidebar
+            </div>
+          ) : (
+            <div className="w-0 dark:bg-secondary-dark-bg">Sidebar</div>
+          )}
+          <div
+            className={`dark-bg-main-bg bg-main-bg min-h-screen w-full ${
+              activeMenu ? "md:ml-72" : "flex-2"
+            }`}
+          ></div>
         </div>
       </BrowserRouter>
     </div>
