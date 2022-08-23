93c146b13b58c20d40f66e3f6b18baed45b9ff16
mkpi-pm <mkpi@pm.me>
2022-08-23 13:57:00 +0100

    add ContextProvider.js with updates to other files for its functionality

--- src/App.js
+++ src/App.js
@@ -5,28 +5,30 @@ import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
 import { Navbar, Footer, Sidebar, ThemeSettings } from "./components";
 import {
+  Ecommerce,
+  Orders,
+  Calendar,
+  Employees,
+  Stacked,
+  Pyramid,
+  Customers,
+  Kanban,
+  Line,
   Area,
   Bar,
-  ColorMapping,
-  Financial,
-  Line,
   Pie,
-  Pyramid,
-  Stacked,
-  Calendar,
+  Financial,
   ColorPicker,
-  Customers,
-  Ecommerce,
+  ColorMapping,
   Editor,
-  Employees,
-  Kanban,
-  Orders,
 } from "./pages";
+
+import { useStateContext } from "./contexts/ContextProvider";
 
 import "./App.css";
 
 const App = () => {
-  const activeMenu = true;
+  const { activeMenu } = useStateContext();
 
   return (
     <div>
@@ -36,7 +38,7 @@ const App = () => {
             <TooltipComponent content="Settings" position="Top">
               <button
                 type="button"
-                className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white"
+                className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray "
                 style={{ background: "blue", borderRadius: "50%" }}
               >
                 <FiSettings />
@@ -53,9 +55,11 @@ const App = () => {
             </div>
           )}
           <div
-            className={`dark-bg-main-bg bg-main-bg min-h-screen w-full ${
-              activeMenu ? "md:ml-72" : "flex-2"
-            }`}
+            className={
+              activeMenu
+                ? "dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  "
+                : "bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 "
+            }
           >
             <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
               <Navbar />
