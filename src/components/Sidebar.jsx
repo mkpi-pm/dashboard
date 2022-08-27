ff3e7e46ccd2aa9fc86f3e5814f520c254c5ac9e
mkpi-pm <mkpi@pm.me>
2022-08-27 18:47:24 +0100

    add fixed bugs in the sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -21,52 +21,58 @@ const Sidebar = () => {
 
   return (
     <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
-      {activeMenu && (
-        <>
-          <div className="flex justify-between items-center">
-            <Link
-              to="/" 
-              onClick={handleCloseSideBar}
-              className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900">
-              <SiShopware />
-              <span>Shoppy</span>
-            </Link>
-            <TooltipComponent content="Menu" position="BottomCenter">
-              <button
-                type="button"
-                onClick={() => setActiveMenu(!activeMenu)}
-                className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
-              >
-                <MdOutlineCancel />
-              </button>
-            </TooltipComponent>
-          </div>
+      {activeMenu && (<>
+        <div className="flex justify-between items-center">
 
-          <div className="mt-10">
-            {links.map((item) => {
+          {/* SiShopware Icon */}
+          <Link
+            to="/" 
+            onClick={handleCloseSideBar}
+            className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900">
+            <SiShopware />
+            <span>Shoppy</span>
+          </Link>
+
+          {/* MdOutlineCancel Icon */}
+          <TooltipComponent content="Menu" position="BottomCenter">
+            <button
+              type="button"
+              onClick={() => setActiveMenu(!activeMenu)}
+              className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
+            >
+              <MdOutlineCancel />
+            </button>
+          </TooltipComponent>
+        </div>
+
+        {/* links list*/}
+        <div className="mt-10">
+          {links.map((item) => {
+            return(
               <div key={item.title}>
                 <p className="text-gray-400 m-3 mt-4 uppercase">
                   {item.title}
                 </p>
                 {item.links.map((link) => {
-                  <NavLink
-                    to={`/${link.name}`}
-                    key={link.name}
-                    onClick={handleCloseSideBar}
-                    className={({isActive}) => isActive ? activeLink : normalLink}
-                  >
-                    {link.icon}
-                    <span className="capitalize">
-                      {link.name}
-                    </span>
-                  </NavLink>
+                  return(
+                    <NavLink
+                      to={`/${link.name}`}
+                      key={link.name}
+                      onClick={handleCloseSideBar}
+                      className={({isActive}) => isActive ? activeLink : normalLink}
+                    >
+                      {link.icon}
+                      <span className="capitalize">
+                        {link.name}
+                      </span>
+                    </NavLink>
+                  )
                 })}
-              </div>
-            })}
-          </div>
-          
-        </>
-      )}
+            </div>
+            )
+          })}
+        </div>
+      </>)}
     </div>
   );
 };
