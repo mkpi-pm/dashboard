d0de1498ded0ca67367b6da2b9c4f54b45beea03
mkpi-pm <mkpi@pm.me>
2022-09-03 13:37:11 +0100

    add ColorPicker app

--- src/pages/ColorPicker.jsx
+++ src/pages/ColorPicker.jsx
@@ -1,9 +1,72 @@
-import React from 'react'
+import React from 'react';
+import { ColorPickerComponent } from '@syncfusion/ej2-react-inputs';
+
+import { Header } from '../components';
+
+// change pen
+const change = (args) => {
+  document.getElementById("preview").style.backgroundColor = args.currentValue.hex;
+}
 
 const ColorPicker = () => {
   return (
-    <div>ColorPicker</div>
+    // CardBlank
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+
+      {/* Header */}
+      <Header category="App" title="ColorPicker" />
+
+      {/* content */}
+      <div className="text-center">
+
+        {/* pen */}
+        <div id="preview" />
+
+        {/* Palette & Picker Content*/}
+        <div className="flex justify-center items-center gap-20 flex-wrap">
+
+          {/* Inline Palette */}
+          <div>
+
+            {/* Palette Header */}
+            <p className="text-2xl font-semibold mt-2 mb-4">Inline Palette</p>
+
+            {/* ColorPickerComponent & props*/}
+            <ColorPickerComponent
+              id="inline-palette"
+              mode="Palette"
+              modeSwitcher={false}
+              inline
+              showButtons={false}
+              change={change}
+             />
+
+          </div>
+
+          {/* Inline Picker */}
+          <div>
+
+            {/* Picker Header */}
+            <p className="text-2xl font-semibold mt-2 mb-4">Inline Picker</p>
+
+            {/* ColorPickerComponent & props*/}
+            <ColorPickerComponent
+              id="inline-picker"
+              mode="Picker"
+              modeSwitcher={false}
+              inline
+              showButtons={false}
+              change={change}
+            />
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
   )
 }
 
-export default ColorPicker
+export default ColorPicker;
