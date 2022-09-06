c75aa071d24448be8fd83479a38aa5dae114160b
mkpi-pm <mkpi@pm.me>
2022-09-06 13:10:13 +0100

    add Line component

--- src/pages/Charts/Line.jsx
+++ src/pages/Charts/Line.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+
+import { Header, LineChart } from "../../components";
 
 const Line = () => {
   return (
-    <div>Line</div>
+        // CardBlank
+        <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+
+          {/* Header */}
+          <Header category="Chart" title="Inflation Rate" />
+  
+          {/* content */}
+          <div className="text-w-full">
+            <LineChart />
+          </div>
+
+        </div>
   )
 }
 
-export default Line
+export default Line;
