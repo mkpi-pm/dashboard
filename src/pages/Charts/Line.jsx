30fb3c9b852e069bc9a7c38939de2e12b160c5a9
mkpi-pm <mkpi@pm.me>
2022-09-07 12:20:37 +0100

    add changed chart category name to Line

--- src/pages/Charts/Line.jsx
+++ src/pages/Charts/Line.jsx
@@ -8,7 +8,7 @@ const Line = () => {
         <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
 
           {/* Header */}
-          <Header category="Chart" title="Inflation Rate" />
+          <Header category="Line" title="Inflation Rate" />
   
           {/* content */}
           <div className="text-w-full">
