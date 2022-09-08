954182d14c01f7952b67e8d8b1bf375798ade1e6
mkpi-pm <mkpi@pm.me>
2022-09-08 15:33:32 +0100

    add Financial chart component

--- src/pages/Charts/Financial.jsx
+++ src/pages/Charts/Financial.jsx
@@ -1,9 +1,70 @@
-import React from 'react'
+import * as React from "react";
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, HiloSeries, Tooltip, DateTime, Zoom, Logarithmic, Crosshair } from '@syncfusion/ej2-react-charts';
 
-const Financial = () => {
-  return (
-    <div>Financial</div>
-  )
+import { financialChartData, FinancialPrimaryXAxis, FinancialPrimaryYAxis } from '../../data/dummy';
+import { useStateContext } from '../../contexts/ContextProvider';
+import { Header } from '../../components';
+
+const date1 = new Date('2017, 1, 1');
+
+function filterValue(value) {
+  if (value.x >= date1) {
+    return value.x, value.high, value.low;
+  }
 }
 
-export default Financial
+const returnValue = financialChartData.filter(filterValue);
+
+const Financial = () => {
+  const { currentMode } = useStateContext();
+
+  return (
+    // CardBlank
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+
+      {/* Header */}
+      <Header category="Chart" title="Financial" />
+
+      <div className="w-full">
+
+        {/* ChartComponent & props*/}
+        <ChartComponent
+          id='charts'
+          primaryXAxis={FinancialPrimaryXAxis}
+          primaryYAxis={FinancialPrimaryYAxis}
+          chartArea={{ border: { width: 0 } }}
+          background={ currentMode === "Dark" ? "#33373E" : "#fff"}
+          tooltip={{ enable: true, shared: true }}
+          crosshair={{ enable: true, lineType: 'Vertical', line: { width: 0 } }}
+        >
+          {/* Functionality */}
+          <Inject services={[HiloSeries, Tooltip, DateTime, Logarithmic, Crosshair, Zoom]}/>
+          
+          {/* Data */}
+          <SeriesCollectionDirective>
+
+            <SeriesDirective
+              dataSource={returnValue}
+              xName='x'
+              yName='low'
+              name='Apple Inc'
+              type='Hilo'
+              low='low'
+              high='high'
+              // fill= 'currentMode'
+            >
+            </SeriesDirective>
+
+            </SeriesCollectionDirective>
+            
+          </ChartComponent>
+            
+      </div>
+
+    </div>
+
+  );
+
+}
+
+export default Financial;
