e6a421e0facf8632857c83c23faab8355f00e317
mkpi-pm <mkpi@pm.me>
2022-09-14 16:58:52 +0100

    add Button refactored

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from "react";
 
-import { useStateContext } from '../contexts/ContextProvider';
+import { useStateContext } from "../contexts/ContextProvider";
 
-const Button = ({ icon, bgColor, color, bgHoverColor, size, text, borderRadius, width}) => {
+const Button = ({ icon, bgColor, color, bgHoverColor, size, text, borderRadius, width }) => {
   const { setIsClicked, initialState } = useStateContext();
 
   return (
     <button
       type="button"
       onClick={() => setIsClicked(initialState)}
-      style={{backgroundColor: bgColor, color, borderRadius }}
+      style={{ backgroundColor: bgColor, color, borderRadius }}
       className={`text-${size} p-3 w-${width} hover:bg-${bgHoverColor}`}>
       {icon} {text}
     </button>
