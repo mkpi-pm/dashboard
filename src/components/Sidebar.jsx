1b9acc1309c3eb25b9104366a7a8b54b1305a1e4
mkpi-pm <mkpi@pm.me>
2022-09-14 17:07:44 +0100

    add Sidebar refactored

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import { Link, NavLink } from "react-router-dom";
 import { SiShopware } from "react-icons/si";
 import { MdOutlineCancel } from "react-icons/md";
@@ -17,66 +17,106 @@ const Sidebar = () => {
   }
 
   const activeLink = "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2";
+  
   const normalLink = "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2";
 
   return (
+    // Sidebar Card
     <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
-      {activeMenu && (<>
-        <div className="flex justify-between items-center">
 
-          {/* SiShopware Icon */}
-          <Link
-            to="/" 
-            onClick={handleCloseSideBar}
-            className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900">
-            <SiShopware />
-            <span>Sample</span>
-          </Link>
+      {/* Active Menu Settings */}
+      {activeMenu && (
+        <>
+          {/* Sidebar Hedline */}
+          <div className="flex justify-between items-center">
 
-          {/* MdOutlineCancel Icon */}
-          <TooltipComponent content="Menu" position="BottomCenter">
-            <button
-              type="button"
-              onClick={() => setActiveMenu(!activeMenu)}
-              className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
-            >
-              <MdOutlineCancel />
-            </button>
-          </TooltipComponent>
-        </div>
+            {/* SiShopware Icon & Title */}
+            <Link
+              to="/" 
+              onClick={handleCloseSideBar}
+              className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900">
 
-        {/* links list*/}
-        <div className="mt-10">
-          {links.map((item) => {
-            return(
+              {/* SiShopware Icon */}
+              <SiShopware />
+
+              {/* Sidebar Title */}
+              <span>Sample</span>
+
+            </Link>
+
+            {/* MdOutline Cancel Icon & button*/}
+            <TooltipComponent content="Menu" position="BottomCenter">
+
+              {/* button */}
+              <button
+                type="button"
+                onClick={() => setActiveMenu(!activeMenu)}
+                className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
+              >
+                {/* MdOutline Cancel Icon */}
+                <MdOutlineCancel />
+
+              </button>
+
+            </TooltipComponent>
+
+          </div>
+
+          {/* links list */}
+          <div className="mt-10">
+
+            {/* Menu titles & links */}
+            {links.map((item) => (
+
+              // Menu titles
               <div key={item.title}>
+
+                {/* single title */}
                 <p className="text-gray-400 m-3 mt-4 uppercase">
                   {item.title}
                 </p>
-                {item.links.map((link) => {
-                  return(
-                    <NavLink
-                      to={`/${link.name}`}
-                      key={link.name}
-                      style={({ isActive }) => ({ backgroundColor: isActive ? currentColor : ""})}
-                      className={({ isActive }) => isActive ? activeLink : normalLink}
-                      onClick={handleCloseSideBar}
 
-                    >
-                      {link.icon}
-                      <span className="capitalize">
-                        {link.name}
-                      </span>
-                    </NavLink>
-                  )
-                })}
-            </div>
-            )
-          })}
-        </div>
-      </>)}
+                {/* Menu links */}
+                {item.links.map((link) => (
+
+                  // Single link name/icon props
+                  <NavLink
+                    to={`/${link.name}`}
+                    key={link.name}
+                    onClick={handleCloseSideBar}
+                    style={({ isActive }) => ({
+                      backgroundColor: isActive ? currentColor : ""
+                    })}
+                    className={({ isActive }) => (
+                      isActive ? activeLink : normalLink
+                    )}
+                  >
+                    {/* Single link icon */}
+                    {link.icon}
+
+                    {/* Single link name */}
+                    <span className="capitalize">
+                      {link.name}
+                    </span>
+
+                  </NavLink>
+
+                ))}
+
+              </div>
+
+            ))}
+
+          </div>
+
+        </>
+      
+      )}
+
     </div>
-  );
-};
+    
+  )
+
+}
 
 export default Sidebar;
