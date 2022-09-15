6f6c6d67fe47d6ae5e257e4cbd3dc447c93e111e
mkpi-pm <mkpi@pm.me>
2022-09-15 16:08:19 +0100

    add refactored and with new comments Navbar and ThemeSettings

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,36 +1,46 @@
-import React, {useEffect} from 'react';
-import { AiOutlineMenu } from 'react-icons/ai';
-import { FiShoppingCart } from 'react-icons/fi';
-import { BsChatLeft } from 'react-icons/bs';
-import { RiNotification3Line } from 'react-icons/ri';
-import { MdKeyboardArrowDown } from 'react-icons/md';
-import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+import React, {useEffect} from "react";
+import { AiOutlineMenu } from "react-icons/ai";
+import { FiShoppingCart } from "react-icons/fi";
+import { BsChatLeft } from "react-icons/bs";
+import { RiNotification3Line } from "react-icons/ri";
+import { MdKeyboardArrowDown } from "react-icons/md";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
 import avatar from "../data/avatar1.jpg";
 import {Cart, Chat, Notification, UserProfile} from ".";
 import {useStateContext} from "../contexts/ContextProvider";
 
 const NavButton = ({title, customFunc, icon, color, dotColor}) => (
+
+  // Navigation components settings
   <TooltipComponent
-    content={title}
+    content={ title }
     position="BottomCenter"
   >
+    {/* Navigation buttons & props */}
     <button
       type="button"
-      onClick={customFunc}
-      style={{color}}
+      onClick={() => customFunc()}
+      style={{ color }}
       className="relative text-xl rounded-full p-3 hover:bg-light-gray"
     >
-      <span style={{background: dotColor}}
+      {/* Navigation dots */}
+      <span
+        style={{ background: dotColor }}
         className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2"
       />
+
+      {/* Navigation icons */}
       {icon}
+
     </button>
+
   </TooltipComponent>
+
 );
 
 const Navbar = () => {
-  const {activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick, screenSize, setScreenSize, currentColor} = useStateContext();
+  const {activeMenu, setActiveMenu, isClicked, handleClick, screenSize, setScreenSize, currentColor} = useStateContext();
 
   useEffect(() => {
     const handleResize = () => setScreenSize(window.innerWidth);
@@ -54,7 +64,8 @@ const Navbar = () => {
 
 
   return (
-    <div className="flex justify-between p-2 md:mx-6 relative">
+    // Navbar elements
+    <div className="flex justify-between p-2 md:ml-6 md:mr-6 relative">
 
       {/* Menu button*/}
       <NavButton
@@ -92,30 +103,40 @@ const Navbar = () => {
           content="Profile"
           position="BottomCenter"
         >
+          {/* Profile Card */}
           <div
             className="flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg"
             onClick={() => handleClick("userProfile")}
           >
+            {/* Profile Image */}
             <img
               className="rounded-full w-8 h-8"
               src={avatar}
             />
+            {/* Profile text */}
             <p>
               <span className="text-gray-400 text-14">Hi, </span> {" "}
               <span className="text-gray-400 font-bold ml-1 text-14">Peter</span>
             </p>
+
+            {/* MdKeyboardArrowDown Component */}
             <MdKeyboardArrowDown className="text-gray-400 text-14" />
+
           </div>
+
         </TooltipComponent>
 
         {isClicked.cart && <Cart />}
         {isClicked.chat && <Chat />}
         {isClicked.notification && <Notification />}
         {isClicked.userProfile && <UserProfile />}
+
       </div>
       
     </div>
-  );
-};
+
+  )
+  
+}
 
 export default Navbar;
--- src/components/ThemeSettings.jsx
+++ src/components/ThemeSettings.jsx
@@ -1,131 +1,126 @@
-import React from 'react';
-import { MdOutlineCancel } from 'react-icons/md';
-import { BsCheck } from 'react-icons/bs';
-import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+import React from "react";
+import { MdOutlineCancel } from "react-icons/md";
+import { BsCheck } from "react-icons/bs";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
-import { themeColors } from '../data/dummy';
-import { useStateContext } from '../contexts/ContextProvider';
+import { themeColors } from "../data/dummy";
+import { useStateContext } from "../contexts/ContextProvider";
 
 const ThemeSettings = () => {
   const { setColor, setMode, currentMode, currentColor, setThemeSettings } = useStateContext();
 
   return (
-    <div>
-      {/* settings shadow */}
-      <div className="bg-half-transparent w-screen fixed nav-item top-0 right-0 ">
+    // settings shadow
+    <div className="bg-half-transparent w-screen fixed nav-item top-0 right-0">
 
-        {/* settings card */}
-        <div className="float-right h-screen dark:text-gray-200 bg-white
-                       dark:bg-[#484B52] w-400">
+      {/* settings card */}
+      <div className="float-right h-screen dark:text-gray-200 bg-white dark:bg-[#484B52] w-400">
 
-          {/* settings title */}
-          <div className="flex justify-between items-center p-4 ml-4">
-            <p className="font-semibold text-lg">Settings</p>
+        {/* settings title */}
+        <div className="flex justify-between items-center p-4 ml-4">
+          <p className="font-semibold text-lg">Settings</p>
 
-            {/* close button */}
-            <button
-              type="button"
+          {/* closing button */}
+          <button
+            type="button"
               style={{ color: "rgb(153, 171, 180)", borderRadius: "50%" }}
               className="text-2xl p-3 hover:drop-shadow-xl hover:bg-light-gray"
               onClick={() => setThemeSettings(false)}
+          >
+            <MdOutlineCancel />
+          </button>
+
+        </div>
+
+        {/* theme options border */}
+        <div className="flex-col border-t-1 border-color p-4 ml-4">
+
+          {/* theme options title */}
+          <p className="font-semibold text-xl">Theme Options</p>
+
+          {/* theme options toggle - light input */}
+          <div className="mt-4">
+            <input
+              type="radio"
+              id="light"
+              name="theme"
+              value="Light"
+              className="cursor-pointer"
+              onChange={setMode}
+              checked={currentMode === "Light"}
+             />
+              
+            {/* theme options light label */}
+            <label
+              htmlFor="light"
+              className="ml-2 text-md cursor-poointer"
             >
-              <MdOutlineCancel />
-            </button>
+              Light
+            </label>
 
           </div>
-
-          {/* theme options border */}
-          <div className="flex-col border-t-1 border-color p-4 ml-4">
-
-            {/* theme options title */}
-            <p className="font-semibold text-xl">Theme Options</p>
-
-            {/* theme options toggle - light input */}
-            <div className="mt-4">
-              <input
-                type="radio"
-                id="light"
-                name="theme"
-                value="Light"
-                className="cursor-pointer"
-                onChange={setMode}
-                checked={currentMode === "Light"}
-               />
-              
-              {/* theme options light label */}
-              <label
-                htmlFor="light"
-                className="ml-2 text-md cursor-poointer"
-              >
-                Light
-              </label>
-
-            </div>
             
-            {/* theme options toggle - dark input */}
-            <div className="mt-4">
-              <input
-                type="radio"
-                id="dark"
-                name="theme"
-                value="Dark"
-                className="cursor-pointer"
-                onChange={setMode}
-                checked={currentMode === "Dark"}
-               />
+          {/* theme options toggle - dark input */}
+          <div className="mt-2">
+            <input
+              type="radio"
+              id="dark"
+              name="theme"
+              value="Dark"
+              className="cursor-pointer"
+              onChange={setMode}
+              checked={currentMode === "Dark"}
+             />
               
-              {/* theme options dark label */}
-              <label
-                htmlFor="dark"
-                className="ml-2 text-md cursor-poointer"
-              >
-                Dark
-              </label>
-
-            </div>
+            {/* theme options dark label */}
+            <label
+              htmlFor="dark"
+              className="ml-2 text-md cursor-poointer"
+            >
+              Dark
+            </label>
 
           </div>
 
-          {/* theme colors border */}
-          <div className="flex-col border-t-1 border-color p-4 ml-4">
+        </div>
 
-            {/* theme colors title */}
-            <p className="font-semibold text-xl">Theme Colors</p>
+        {/* theme colors border */}
+        <div className="flex-col border-t-1 border-color p-4 ml-4">
 
-            {/* content position & spacing*/}
-            <div className="flex gap-3">
+          {/* theme colors title */}
+          <p className="font-semibold text-xl">Theme Colors</p>
 
-              {themeColors.map((item, index) => (
+          {/* content position & spacing*/}
+          <div className="flex gap-3">
 
-                // content names
-                <TooltipComponent
-                  key={index}
-                  content={item.name}
-                  position="TopCenter">
+            {themeColors.map((item, index) => (
 
-                    {/* content positions */}
-                    <div className="relative mt-2 cursor-pointer flex gap-5 items-center">
+              // content names
+              <TooltipComponent
+                key={index}
+                content={item.name}
+                position="TopCenter"
+              >
+                {/* content positions */}
+                <div className="relative mt-2 cursor-pointer flex gap-5 items-center">
 
-                      {/* content shape & color toggle */}
-                      <button
-                        type="button"
-                        style={{ backgroundColor: item.color}}
-                        className="h-10 w-10 rounded-full cursor-pointer"
-                        onClick={() => setColor(item.color)}
-                      >
+                  {/* content shape & color toggle */}
+                  <button
+                    type="button"
+                    style={{ backgroundColor: item.color}}
+                    className="h-10 w-10 rounded-full cursor-pointer"
+                    onClick={() => setColor(item.color)}
+                  >
+                    <BsCheck
+                      className={`ml-2 text-2xl text-white ${item.color === currentColor ? "block" : "hidden"}`} />
 
-                        <BsCheck
-                          className={`ml-2 text-2xl text-white ${item.color === currentColor ? "block" : "hidden"}`} />
+                  </button>
 
-                      </button>
-
-                    </div>
+                </div>
                   
-                </TooltipComponent>
+              </TooltipComponent>
 
-              ))}
-
-            </div>
+            ))}
 
           </div>
 
@@ -136,6 +131,7 @@ const ThemeSettings = () => {
     </div>
 
   )
+  
 }
 
 export default ThemeSettings;
