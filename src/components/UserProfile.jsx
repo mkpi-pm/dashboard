de9822f3336e80e6457e352ee7113eaa5d9cd4a5
mkpi-pm <mkpi@pm.me>
2022-09-15 15:50:44 +0100

    add UserProfile component

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,9 +1,104 @@
-import React from 'react'
+import React from "react";
+import { MdOutlineCancel } from "react-icons/md";
+
+import { Button } from ".";
+import { userProfileData } from "../data/dummy";
+import { useStateContext } from "../contexts/ContextProvider";
+import avatar from "../data/avatar1.jpg";
 
 const UserProfile = () => {
+  const { currentColor } = useStateContext();
+
   return (
-    <div>UserProfile</div>
+    // User Profile Card
+    <div className="nav-item absolute right-1 top-16 bg-white dark:bg-[#42464D] p-8 rounded-lg w-96">
+
+      {/* User Profile Headline */}
+      <div className="flex justify-between items-center">
+
+        {/* User Profile Text */}
+        <p className="font-semibold text-lg dark:text-gray-200">User Profile</p>
+
+        {/* Closing Button */}
+        <Button
+          icon={<MdOutlineCancel />}
+          color="rgb(153, 171, 180)"
+          bgHoverColor="light-gray"
+          size="2xl"
+          borderRadius="50%"
+        />
+
+      </div>
+
+      {/* User Data */}
+      <div className="flex gap-5 items-center mt-6 border-color border-b-1 pb-6">
+
+        {/* User Image */}
+        <img
+          className="rounded-full h-24 w-24"
+          src={avatar}
+          alt="user-profile"
+        />
+
+        <div>
+          {/* User Info */}
+          <p className="font-semibold text-xl dark:text-gray-200"> Peter Roberts </p>
+          <p className="text-gray-500 text-sm dark:text-gray-400">  Administrator   </p>
+          <p className="text-gray-500 text-sm font-semibold dark:text-gray-400"> info@sdg.com </p>
+        </div>
+
+      </div>
+
+      {/* Profile Inbox Tasks */}
+      <div>
+        {/* List */}
+        {userProfileData.map((item, index) => (
+
+          // Single data card
+          <div key={index} className="flex gap-5 border-b-1 border-color p-4 hover:bg-light-gray cursor-pointer  dark:hover:bg-[#42464D]">
+
+            {/* Icon & props*/}
+            <button
+              type="button"
+              style={{ color: item.iconColor, backgroundColor: item.iconBg }}
+              className="text-xl rounded-lg p-3 hover:bg-light-gray"
+            >
+              {item.icon}
+            </button>
+
+            {/* Title & Description */}
+            <div>
+              {/* Title */}
+              <p className="font-semibold dark:text-gray-200 ">{item.title}</p>
+
+              {/* Description */}
+              <p className="text-gray-500 text-sm dark:text-gray-400"> {item.desc} </p>
+            </div>
+
+          </div>
+
+        ))}
+
+      </div>
+
+      {/* Logout button card */}
+      <div className="mt-5">
+        
+        {/* Button */}
+        <Button
+          color="white"
+          bgColor={currentColor}
+          text="Logout"
+          borderRadius="10px"
+          width="full"
+        />
+
+      </div>
+
+    </div>
+
   )
+
 }
 
-export default UserProfile
+export default UserProfile;
