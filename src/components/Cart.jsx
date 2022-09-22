677e9d333f83268066171c716a8bd09479e8d23b
mkpi-pm <mkpi@pm.me>
2022-09-22 17:49:29 +0100

    add hovering to cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -42,7 +42,7 @@ const Cart = () => {
             <div>
 
               {/* Cart Item Info Card*/}
-              <div className="flex items-center leading-8 gap-5 border-b-1 border-color dark:border-gray-600 p-4">
+              <div className="flex items-center leading-8 gap-5 border-b-1 border-color dark:border-gray-600 p-4 hover:bg-light-gray cursor-pointer">
 
                 {/* Item Image */}
                 <img className="rounded-lg h-80 w-24" src={item.image} alt="" />
