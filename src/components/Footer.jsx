da3aaef5d7bef06c5afb4cf7c817024c287d464a
mkpi-pm <mkpi@pm.me>
2022-09-28 12:58:11 +0100

    add wrap for footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="mt-12">
-      <p className="dark:text-gray-200 text-gray-700 text-center ml-20">
+      <p className="dark:text-gray-200 text-gray-700 text-center ml-20 flex-wrap">
         Copyright © 2022 SoliDeoGloria Inc. All Rights Reserved.
       </p>
     </div>
